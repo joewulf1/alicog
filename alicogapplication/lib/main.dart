1ad1cee82a795a3ff0088d3a779b96de7b1e0dfe
joewulf1 <joe.hiking@gmail.com>
2022-12-04 17:34:15 -0600

    Added Calendar

--- alicogapplication/lib/main.dart
+++ alicogapplication/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -42,6 +43,17 @@ class _MyHomePageState extends State<MyHomePage> {
     var screen = MediaQuery.of(context).size;
     var widthScreen = screen.width;
     var heightScreen = screen.height;
+    List<Meeting> _getDataSource() {
+      final List<Meeting> meetings = <Meeting>[];
+      final DateTime today = DateTime.now();
+      final DateTime startTime =
+          DateTime(today.year, today.month, today.day, 9);
+      final DateTime endTime = startTime.add(const Duration(hours: 2));
+      meetings.add(Meeting(
+          'Conference', startTime, endTime, const Color(0xFF0F8644), false));
+      return meetings;
+    }
+
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
@@ -55,12 +67,18 @@ class _MyHomePageState extends State<MyHomePage> {
                 children: [
                   Text("Calendar"),
                   SizedBox(
-                    width: widthScreen * .33,
-                    height: heightScreen * .80,
-                    child: DecoratedBox(
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 175, 76, 76))),
-                  ),
+                      width: widthScreen * .33,
+                      height: heightScreen * .80,
+                      child: SfCalendar(
+                        view: CalendarView.month,
+                        dataSource: MeetingDataSource(_getDataSource()),
+                        // by default the month appointment display mode set as Indicator, we can
+                        // change the display mode as appointment using the appointment display
+                        // mode property
+                        monthViewSettings: const MonthViewSettings(
+                            appointmentDisplayMode:
+                                MonthAppointmentDisplayMode.appointment),
+                      )),
                 ],
               ),
             ),
@@ -154,6 +172,69 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ));
+  }
+}
+
+class Meeting {
+  /// Creates a meeting class with required details.
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+
+  /// Event name which is equivalent to subject property of [Appointment].
+  String eventName;
+
+  /// From which is equivalent to start time property of [Appointment].
+  DateTime from;
+
+  /// To which is equivalent to end time property of [Appointment].
+  DateTime to;
+
+  /// Background which is equivalent to color property of [Appointment].
+  Color background;
+
+  /// IsAllDay which is equivalent to isAllDay property of [Appointment].
+  bool isAllDay;
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  /// Creates a meeting data source, which used to set the appointment
+  /// collection to the calendar
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return _getMeetingData(index).from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return _getMeetingData(index).to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return _getMeetingData(index).eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return _getMeetingData(index).background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return _getMeetingData(index).isAllDay;
+  }
+
+  Meeting _getMeetingData(int index) {
+    final dynamic meeting = appointments![index];
+    late final Meeting meetingData;
+    if (meeting is Meeting) {
+      meetingData = meeting;
+    }
+
+    return meetingData;
   }
 }
 
